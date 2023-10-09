7739ab92ab73830620bb48f9ce5043a6ef4c61ad
YSW2 <jrgyber@gmail.com>
2023-10-09 15:56:50 +0900

    TrendFollowing Commit

--- BollingerBand.py
+++ BollingerBand.py
@@ -10,7 +10,6 @@ df['stddev'] = df['close'].rolling(window=20).std()
 df['upper'] = df['MA20'] + (df['stddev'] * 2)
 df['lower'] = df['MA20'] - (df['stddev'] * 2)
 df['bandwidth'] = (df['upper'] - df['lower']) / df['MA20'] * 100
-df['PB'] = (df['close'] - df['lower']) / (df['upper'] - df['lower'])
 df = df[19:]
 
 plt.figure(figsize=(9, 8))
@@ -20,11 +19,7 @@ plt.plot(df.index, df['upper'], 'r--', label='Upper band')
 plt.plot(df.index, df['MA20'], 'k--', label='Moving average 20')
 plt.plot(df.index, df['lower'], 'c--', label='Lower band')
 plt.fill_between(df.index, df['upper'], df['lower'], color='0.9')
-for i in range(len(df.close)):
-    if df.PB.values[i] > 0.8 and df.MFI10.values[i] > 80:
-        plt.plot(df.index.values[i], df.close.values[i], 'r^')
-    elif df.PB.values[i] > 0.8 and df.MFI10.values[i] < 20:
-        plt.plot(df.index.values[i], df.close.values[i], 'bv')
+
 plt.legend(loc='best')
 plt.title(f'{market_name} Bollinger Band (20 day, 2 std)')
 
